9670f7312bdf509fe7a14bcc319b74afb06d60d8
Oleksiy Pastukhov <oleksiy.pastukhov@gmail.com>
2020-04-15 02:19:10 +0300

    Extended tokenizer possibilities

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { en, no } from './stopwords';
+import allStopwords from './stopwords/all';
 import nGram from './helpers/nGram';
 
 const stopWordsByLangMap = { en, no };
 const defaultLang = 'en';
+const emailRegExp = new RegExp(/\S+[a-z0-9]@[a-z0-9\.]+/img);
 const splittingRegExp = new RegExp(/[&/\\#.,!?+() $~%:*<>{}]/g);
 
-const getTokens = (query, lang) => {
+const getTokensByLang = (query, lang) => {
   const stopwords = lang ? stopWordsByLangMap[lang] : defaultLang;
   const queryWords = query && query.split(splittingRegExp);
   const filteredWords = queryWords && queryWords.filter(word => word !== '');
@@ -18,4 +20,21 @@ const getTokens = (query, lang) => {
 const getBigram = tokens => nGram.bigram(tokens);
 const getTrigram = tokens => nGram.trigram(tokens);
 
-export { getTokens, getBigram, getTrigram };
+const getTokens = (query) => {
+  const stopwords = allStopwords;
+  const extractedEmails = query.match(emailRegExp);
+  const queryWords = query && query.split(splittingRegExp);
+  const filteredWords = queryWords && queryWords.filter(word => word !== '');
+  const wordsFormatted = filteredWords && filteredWords.map(word => word.toLowerCase());
+  const tokens = wordsFormatted && wordsFormatted.filter(word => !stopwords[word]);
+  const allTokens = extractedEmails && tokens ? [...extractedEmails, ...tokens] : tokens;
+  const bigrams = getBigram(allTokens);
+  const trigrams = getTrigram(allTokens);
+  const resultTokens = allTokens && [...allTokens, ...bigrams, ...trigrams];
+
+  return resultTokens ? [...new Set(resultTokens)] : [];
+};
+
+export {
+  getTokens, getTokensByLang, getBigram, getTrigram
+};
